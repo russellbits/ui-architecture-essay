7dbfc6065a191e37d6a659efeabce35e25227936
Russell Warner <rewarner@russellbits.com>
2022-12-21 15:05:19 -0500

    Initial commit and title component.

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import Title from './lib/components/Title.svelte'
+</script>
+
+<main>
+  <Title/>
+
+<section id="introduction">
+
+</section>
+</main>
+
+<style>
+main {
+  /* display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  gap: 1em;*/
+}
+</style>
---
+++ src/lib/components/Title.svelte
@@ -0,0 +1,38 @@
+<script></script>
+
+<section id="title">
+  <div id="title_line_1">User Interface</div>
+  <div id="title_line_2">Architecture</div>
+  <div id="byline">by Russell Warner</div>
+</section>
+
+<style>
+section#title {
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  gap: 1em;
+  margin-top: 108px;
+}
+
+#title_line_1 {
+  grid-column: 2 / 9;
+  font-size: 57px;
+  text-transform: uppercase;
+  line-height: 12px;
+}
+
+#title_line_2 {
+  grid-column: 2 / 9;
+  font-size: 60px;
+  font-weight: bold;
+  line-height: 73px;
+  text-transform: uppercase;
+}
+
+#byline {
+  grid-column: 5 / 9;
+  font-size: 24px;
+  font-weight: 400;
+  text-align: right;
+}
+</style>
